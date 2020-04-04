442e882f1aa29663eb95c48a9beefbd21e974296
111hunter <1621209357@qq.com>
2020-04-04 20:28:53 +0800

    add auth

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,47 @@
+import {
+    Injectable,
+    InternalServerErrorException,
+    UnauthorizedException,
+    ConflictException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { LoginDTO, RegisterDTO } from 'src/models/user.model';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserEntity } from 'src/entities/user.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class AuthService {
+    constructor(
+        @InjectRepository(UserEntity) private userRepo: Repository<UserEntity>,
+        private jwtService: JwtService,
+    ) { }
+
+    async register(credentials: RegisterDTO) {
+        try {
+            const user = this.userRepo.create(credentials);
+            await user.save();
+            return { user: { ...user.toJSON() } };
+        } catch (err) {
+            if (err.code === '23505') {
+                throw new ConflictException('Username has already been taken');
+            }
+            throw new InternalServerErrorException();
+        }
+    }
+
+    async login({ email, password }: LoginDTO) {
+        try {
+            const user = await this.userRepo.findOne({ where: { email } });
+            const isValid = await user.comparePassword(password);
+            if (!isValid) {
+                throw new UnauthorizedException('Invalid credentials');
+            }
+            const payload = { username: user.username };
+            const token = this.jwtService.sign(payload);
+            return { user: { ...user.toJSON(), token } };
+        } catch (err) {
+            throw new UnauthorizedException('Invalid credentials');
+        }
+    }
+}
---
+++ src/models/user.model.ts
@@ -0,0 +1,23 @@
+import { IsEmail, IsString, MinLength, MaxLength } from 'class-validator';
+
+export class LoginDTO {
+    @IsEmail()
+    @IsString()
+    @MinLength(4)
+    email: string;
+
+    @IsString()
+    @MinLength(4)
+    password: string;
+}
+
+export class RegisterDTO extends LoginDTO {
+    @IsString()
+    @MinLength(4)
+    @MaxLength(20)
+    username: string;
+}
+
+export interface AuthPayload {
+    username: string;
+}
